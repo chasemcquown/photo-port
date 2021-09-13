8e768e805dfa241546b28a72514fd8010b350d6b
chasem <cpmcquown@gmail.com>
2021-09-13 16:56:11 -0700

    adds tests for about and nav components

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+afterEach(cleanup);
+
+// will be testing the nav component
+describe('Nav component', () => {
+
+    // baseline test
+    it('renders', () => {
+        render(<Nav />);
+    });
+
+    // snapshot test
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
+        // assert value comparison
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+});
+
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+      const { getByTestId } = render(<Nav />);
+      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+      expect(getByTestId('about')).toHaveTextContent('About me');
+    });
+});
